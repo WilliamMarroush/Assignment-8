8cd785e881958fa3e66ab0d1c520a3ab046e00fe
William Marroush <marroushw@yahoo.com>
2022-01-13 00:28:48 -0500

    Still trying to get Debit List to work

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -2,16 +2,24 @@ import React,{Component,useState} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
-function Debits() {
-    let debtotal;
-    let value1=null;
-    const debtlist=["key1",value1];
+class Debits extends Component{
+    constructor(){
+        super();
+        this.state={
+            debittrans:{
+                debitamt:0,
+                debitdesc:""
+            }
+        }
+    }
 
-    const handleSubmit = (e)=>{
+    handleSubmit = (e)=>{
         let amt = e.amt_dsc.Amount.value;
         let dsc = e.amt_dsc.Description.value;
-        debtlist.push(dsc,amt);
+        this.setState(this.state.dbtamt,amt);
+        this.setState(this.state.dbtdsc,dsc);
     }
+    render(){
         return(
             <div>
                 <div className="d-flex flex-column justify-content-center">
@@ -23,26 +31,18 @@ function Debits() {
                         <input className="Amount" type="text"/><br></br>
                         <label>Description:</label>
                         <input className="Description" type="text"/><br/>
-                        <button className="Add to Debit List" onClick={handleSubmit}>Submit</button>
+                        <button className="Add to Debit List" onClick={this.handleSubmit}>Submit</button>
                     </form>
                     <Link to="/">HomePage</Link>
                     </div>
                 </div>
                 <div className="d-flex flex-column justify-content-right" id="debtList">
                     <h2><u>List of Debits</u></h2>
-                    {
-                        Object.entries(debtlist).forEach(([key,value])=>{
-                            return(
-                                <div>
-                                    Description:  {key}<br/>
-                                    Debit Amount: ${value}
-                                </div>
-                            )
-                        })
-                    }
+                    {this.state.debittrans.debitamt}
+                    {this.state.debittrans.debitdesc}
                 </div>
             </div>
-        )
+        )}
 
 
 }
